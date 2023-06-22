35bafd4da77e6e85753632d455f28d1e4f5fbfbd
Sudha8861 <sudha.a@atop.com.tw>
2023-06-22 11:00:44 +0530

    changes to dash board

--- src/renderer/src/pages/DashboardPage.jsx
+++ src/renderer/src/pages/DashboardPage.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import DeviceSummary from '../components/dashboard/DeviceSummary'
 import DiskSpceSummary from '../components/dashboard/DiskSpaceSummary'
 import { Card, Col, Row } from 'antd'
 import TrapGraphSummary from '../components/dashboard/TrapGraphSummary'
-import SyslogGraph1 from '../components/dashboard/SyslogGraph1'
+import SyslogGraph from '../components/dashboard/SyslogGraph'
 import EventSummary from '../components/dashboard/EventSummary'
 import EventLog from '../components/dashboard/EventLog'
 import EventList from '../components/dashboard/EventList'
@@ -29,7 +30,7 @@ const DashboardPage = () => {
               title="Disk Space Utilization Summary"
               bordered={false}
               size="small"
-              style={{ height: 210 }}
+              style={{ height: 215 }}
               bodyStyle={{ padding: '5px' }}
             >
               <DiskSpceSummary />
@@ -40,7 +41,7 @@ const DashboardPage = () => {
               title="SNMP Trap Message Count"
               bordered={false}
               size="small"
-              style={{ height: 210 }}
+              style={{ height: 215 }}
               bodyStyle={{ padding: '5px' }}
             >
               <TrapGraphSummary />
@@ -65,7 +66,7 @@ const DashboardPage = () => {
             <Card
               title="EventGraph"
               bordered={false}
-              style={{ height: 210 }}
+              style={{ height: 215 }}
               bodyStyle={{ padding: '5px' }}
             >
               <EventLog />
@@ -76,10 +77,10 @@ const DashboardPage = () => {
               title="Syslog Message"
               bordered={false}
               size="small"
-              style={{ height: 210 }}
+              style={{ height: 250 }}
               bodyStyle={{ padding: '5px' }}
             >
-              <SyslogGraph1 />
+              <SyslogGraph />
             </Card>
           </Col>
         </Row>
