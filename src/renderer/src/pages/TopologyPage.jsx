9820555b5fb9a86318750e0d7a2f3c4c4b8549bf
Dileep Kumar C B <dileep.k@atop.com.tw>
2023-06-24 11:55:49 +0530

    Created TopologySlice and Topology Main Page Layout

--- src/renderer/src/pages/TopologyPage.jsx
+++ src/renderer/src/pages/TopologyPage.jsx
@@ -1,7 +1,14 @@
+import Card from 'antd/es/card/Card'
 import React from 'react'
 
 const TopologyPage = () => {
-  return <div>TopologyPage</div>
+  return (
+    <Card bordered={false} title="Device Topology">
+      <p>TopologyToolbarContainer</p>
+      <p>TopologyGraphContainer</p>
+      <p>TopologyAddModalContainer</p>
+    </Card>
+  )
 }
 
 export default TopologyPage
