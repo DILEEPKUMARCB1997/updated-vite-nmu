1b6056fcfaf983b73313a01075d6edc2273f25ae
Dileep Kumar C B <dileep.k@atop.com.tw>
2023-08-22 11:51:46 +0530

    corrections

--- src/renderer/src/components/dialogs/eventHistoryDialog/EventHistoryDialog.jsx
+++ src/renderer/src/components/dialogs/eventHistoryDialog/EventHistoryDialog.jsx
@@ -130,6 +130,7 @@ const EventHistoryDialog = ({ onClose }) => {
 
         <Divider style={{ margin: '10px 0px' }} />
         <Table
+          style={{ height: '50%', tableLayout: 'auto' }}
           loading={tableLoading}
           rowKey="eventId"
           bordered
@@ -143,7 +144,7 @@ const EventHistoryDialog = ({ onClose }) => {
             pageSizeOptions: [25, 50, 75, 100],
             showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
           }}
-          scroll={{ y: 'calc(80vh - 165px)', x: 1500 }}
+          scroll={{ y: 'calc(80vh - 165px)' }}
         />
       </Modal>
     </ConfigProvider>
