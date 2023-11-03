1edb656bd3d38f2a6a3264f7d16d73f49e5f6b2c
Sudha8861 <sudha.a@atop.com.tw>
2023-11-03 12:22:40 +0530

    correction in syslog and SNMP

--- src/renderer/src/components/dialogs/BackupRestoreDialog/FileList/FileList.jsx
+++ src/renderer/src/components/dialogs/BackupRestoreDialog/FileList/FileList.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-<<<<<<< HEAD
 import { Card, Checkbox, Row, Table, Typography, Button, theme, List, ConfigProvider } from 'antd'
-=======
-import { Card, Checkbox, Row, Table, Typography, Button, theme, List } from 'antd'
 
->>>>>>> 81b109078edba65a65a467fbbffe92e503266332
 import { CloseOutlined, StarOutlined } from '@ant-design/icons'
 import {
   backupRestoreSelector,
@@ -40,67 +36,66 @@ const FileList = () => {
   }
 
   return (
-      <Card
-        title="Files"
+    <Card
+      title="Files"
+      size="small"
+      bordered={false}
+      style={{
+        height: '450px',
+        borderRadius: '4px',
+        boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.14), 0px 7px 10px -5px rgba(0, 0, 0, 0.4)'
+      }}
+      headStyle={{ backgroundColor: token.colorPrimaryBorder }}
+    >
+      <List
         size="small"
-        bordered={false}
-        style={{
-          height: '450px',
-          borderRadius: '4px',
-          boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.14), 0px 7px 10px -5px rgba(0, 0, 0, 0.4)'
-        }}
-        headStyle={{ backgroundColor: token.colorPrimaryBorder }}
-      >
-        <List
-          size="small"
-          split={false}
-          itemLayout="horizontal"
-          dataSource={files}
-          renderItem={(item) => (
-            console.log('item', item),
-            (
-              <ListItem
-                actions={[
-                  mode === 'backup' && (
-                    <Button
-                      size="small"
-                      disabled={isTaskRunning}
-                      style={{ color: 'red' }}
-                      icon={<CloseOutlined></CloseOutlined>}
-                      onClick={handleDeleteFileButtonOnClick(item)}
+        split={false}
+        itemLayout="horizontal"
+        dataSource={files}
+        renderItem={(item) => (
+          console.log('item', item),
+          (
+            <ListItem
+              actions={[
+                mode === 'backup' && (
+                  <Button
+                    size="small"
+                    disabled={isTaskRunning}
+                    style={{ color: 'red' }}
+                    icon={<CloseOutlined></CloseOutlined>}
+                    onClick={handleDeleteFileButtonOnClick(item)}
+                  />
+                )
+              ]}
+            >
+              <ListItemMeta
+                title={item}
+                style={{ marginBottom: '0px' }}
+                avatar={
+                  mode === 'restore' && (
+                    <Checkbox
+                      disabled={isTaskRunning || isRestoreFisish}
+                      checked={restoreFile === item}
+                      onChange={handleFileCheckboxOnChange(item)}
                     />
                   )
-                ]}
-              >
-                <ListItemMeta
-                  title={item}
-                  style={{ marginBottom: '0px' }}
-                  avatar={
-                    mode === 'restore' && (
-                      <Checkbox
-                        disabled={isTaskRunning || isRestoreFisish}
-                        checked={restoreFile === item}
-                        onChange={handleFileCheckboxOnChange(item)}
-                      />
-                    )
-                  }
-                />
-              </ListItem>
-            )
-          )}
-          pagination={{
-            type: 'bottom',
-            align: 'center',
-            showQuickJumper: true,
-            size: 'small',
-            total: files.length,
-            defaultPageSize: 5,
-            pageSizeOptions: [5, 10, 15, 20],
-            showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-          }}
-        />
-      </Card>
-
+                }
+              />
+            </ListItem>
+          )
+        )}
+        pagination={{
+          type: 'bottom',
+          align: 'center',
+          showQuickJumper: true,
+          size: 'small',
+          total: files.length,
+          defaultPageSize: 5,
+          pageSizeOptions: [5, 10, 15, 20],
+          showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
+        }}
+      />
+    </Card>
   )
 }
 
