cc4748197a85cd638fcc7f82abf88c85a4335efa
Dileep Kumar C B <dileep.k@atop.com.tw>
2023-12-14 17:18:07 +0530

    added test cases

--- src/renderer/src/components/dialogs/NetworkSettingsDialog/DeviceList/DeviceList.test.js
+++ src/renderer/src/components/dialogs/NetworkSettingsDialog/DeviceList/DeviceList.test.js
@@ -2,44 +2,84 @@ import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { expect, test, describe, jest } from '@jest/globals'
-import { store } from '../../../app/store'
 import '@testing-library/jest-dom'
 import DeviceList from './DeviceList'
+import { store } from '../../../../app/store'
 
-test('should render table in device list card of network setting dialog', () => {
-  window.matchMedia = jest.fn().mockImplementation((query) => ({
-    matches: query !== '(min-width: 240px) and (max-width: 767px)',
-    media: '',
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn()
-  }))
+describe('testing Device list (NetworkSettingDialog)', () => {
+  test('should render device list card', () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: query !== '(min-width: 240px) and (max-width: 767px)',
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }))
 
-  render(
-    <Provider store={store}>
-      <DeviceList />
-    </Provider>
-  )
-  const networkSettingDeviceTable = screen.getByTestId('networkSettingDeviceTable')
-  fireEvent.click(networkSettingDeviceTable)
-  expect(networkSettingDeviceTable).toBeInTheDocument()
-})
+    render(
+      <Provider store={store}>
+        <DeviceList />
+      </Provider>
+    )
+    const card = screen.getByTestId('networkSettingDeviceTable')
+    expect(card).toBeInTheDocument()
+  })
+  test('should render table in device list card of network setting dialog', () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: query !== '(min-width: 240px) and (max-width: 767px)',
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }))
 
-test('should render calculate button', () => {
-  window.matchMedia = jest.fn().mockImplementation((query) => ({
-    matches: query !== '(min-width: 240px) and (max-width: 767px)',
-    media: '',
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn()
-  }))
+    render(
+      <Provider store={store}>
+        <DeviceList />
+      </Provider>
+    )
+    const networkSettingDeviceTable = screen.getByRole('table')
+    fireEvent.click(networkSettingDeviceTable)
+    expect(networkSettingDeviceTable).toBeInTheDocument()
+  })
 
-  render(
-    <Provider store={store}>
-      <DeviceList />
-    </Provider>
-  )
-  const calculateButton = screen.getByRole('button')
-  fireEvent.click(calculateButton)
-  expect(calculateButton).toBeInTheDocument()
+  test('should render start address input field', () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: query !== '(min-width: 240px) and (max-width: 767px)',
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }))
+
+    render(
+      <Provider store={store}>
+        <DeviceList />
+      </Provider>
+    )
+
+    const startAddressInput = screen.getByTitle('Start Address')
+    fireEvent.click(startAddressInput)
+    expect(startAddressInput).toBeInTheDocument()
+  })
+
+  test('should render calculate button', () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: query !== '(min-width: 240px) and (max-width: 767px)',
+      media: '',
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }))
+
+    render(
+      <Provider store={store}>
+        <DeviceList />
+      </Provider>
+    )
+    const calculateButton = screen.getByTestId('calculateButton')
+    fireEvent.click(calculateButton)
+    expect(calculateButton).toBeInTheDocument()
+    expect(calculateButton).toBeDisabled()
+  })
 })
--- src/renderer/src/components/dialogs/NetworkSettingsDialog/NetworkSettingDialog.test.js
+++ src/renderer/src/components/dialogs/NetworkSettingsDialog/NetworkSettingDialog.test.js
@@ -24,3 +24,37 @@ test('should render network setting dialog', () => {
   fireEvent.click(networkSettingModal)
   expect(networkSettingModal).toBeInTheDocument()
 })
+
+test('should render start button', () => {
+  window.matchMedia = jest.fn().mockImplementation((query) => ({
+    matches: query !== '(min-width: 240px) and (max-width: 767px)',
+    media: '',
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn()
+  }))
+
+  render(
+    <Provider store={store}>
+      <NetworkSettingDialog />
+    </Provider>
+  )
+  const startButton = screen.getByRole('button', { name: 'start' })
+  fireEvent.click(startButton)
+  expect(startButton).toBeInTheDocument()
+  expect(startButton).toBeDisabled()
+})
+
+test('should call handleStartButtonClick on clicking start button', () => {
+  const onOk = jest.fn()
+
+  render(
+    <Provider store={store}>
+      <NetworkSettingDialog onOk={onOk} />
+    </Provider>
+  )
+
+  const startButtonClick = screen.getByRole('button', { name: 'start' })
+  fireEvent.click(startButtonClick)
+  expect(startButtonClick).toHaveBeenCalled()
+})
